a04e3ae15fac576abb84357320e5ebb5c873c982
¨hbonavota¨ <hbonavota@gmail.com>
2021-06-11 19:41:01 +0200

    add readme

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+const Button= (props)=>{
+     const handleClick = () => {
+        props.clickHandler(props.name);
+      };
+    
+      const className = [
+        "component-button",
+        props.orange ? "orange" : "",
+        props.wide ? "wide" : "",
+      ];
+
+      return (
+        <div className={className.join(" ").trim()}>
+          <button onClick={handleClick}>{props.name}</button>
+        </div>
+      );
+}
+
+Button.propTypes = {
+    name: PropTypes.string,
+    orange: PropTypes.bool,
+    wide: PropTypes.bool,
+    clickHandler: PropTypes.func,
+  };
+
+export default Button;
---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Display.css";
+
+const Display= (props)=>{
+    return (
+      <div className="component-display">
+        <div>{props.value}</div>
+      </div>
+    );
+}
+    
+Display.propTypes = {
+    value: PropTypes.string,
+};
+
+export default Display;
